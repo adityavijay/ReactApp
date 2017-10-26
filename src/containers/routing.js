7802ecd43d92c336fdf5335f23a0c945b24360a9
aditya vijay <aditya.vijay@islington.gov.uk>
2017-10-26 15:13:21 +0100

    all working routing

--- src/containers/routing.js
+++ src/containers/routing.js
@@ -6,18 +6,37 @@ var NavRoute = ()=>{
 	return <Router>
 				<div>
 					<ul>
-						<li><Link to="/dist">Home</Link></li>
-						<li><Link to="/dist/about-us">About us</Link></li>
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/about-us">About us</Link></li>
+						<li><Link to="/topics">Topics</Link></li>
 					</ul>
 						
 					<hr/>
-					<Route exact path="/dist" component={Home}/>
-					<Route path="/dist/about-us" component={Aboutus}/>
+					
+					<Route  path="/dist" render={()=>(<div>Please click on above link</div>)}/>
+					<Route exact path="/" component={Home}/>
+					<Route path="/about-us" component={Aboutus}/>
+					<Route path="/topics" component={Topics}/>
 				</div>
 			</Router>
 	
 }, Home=()=>(<div>Home info</div>),
 
-Aboutus=()=>(<div>Aboutus info</div>);
+Aboutus=()=>(<div>Aboutus info</div>),
+
+Topics= ({match})=>{
+return 	<div>
+			<ul>
+				<li><Link to={`${match.url}/WorldNews`}>World News</Link></li>
+				<li><Link to={`${match.url}/CountryNews`}>Country News</Link></li>
+				<li><Link to={`${match.url}/LocalNews`}>Local News</Link></li>
+			</ul>
+			<Route path={match.url+"/:topicId"} component={Topic}/>
+			<Route exact path={match.url} render={()=>(<div>Type the news, you want to see</div>)}/>
+		</div>		
+},
+Topic = ({match})=>{
+	return <div>{match.params.topicId}</div>
+};
 
 export default NavRoute;
